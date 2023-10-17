3f613329888323762895dd82c312bba9a3f81cb8
Oleg <zmey3854@gmail.com>
2023-10-17 14:46:02 +0300

    task14

---
+++ src/task14.js
@@ -0,0 +1,7 @@
+const name1 = 'Имя: ';
+const name = 'John\n';
+const surname1 = 'Фамилия: ';
+const surname = 'Doe\n';
+const year1 = 'Возраст: ';
+const year = '25 лет'
+console.log(name1 + name + surname1 + surname + year1 + year);
